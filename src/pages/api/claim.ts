f9aa2b59daa7ca7b3f931db9a9905014086cecfa
Josef Vacek <vacekj@outlook.com>
2022-06-26 04:08:54 -0400

    feat:  minting

--- src/pages/api/claim.ts
+++ src/pages/api/claim.ts
@@ -1,10 +1,15 @@
+import { createSvg } from "@/createSvg";
 import nftContractAbi from "@/nftContractAbi.json";
-import { JsonRpcSigner } from "@ethersproject/providers";
-import { ethers, VoidSigner, Wallet } from "ethers";
+import { ethers, Wallet } from "ethers";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { Blob, NFTStorage } from "nft.storage";
 
+import { Interface } from "@ethersproject/abi";
+
+import abi from "../../nftContractAbi.json";
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
 const API_KEY = process.env.NEXT_NFT_STORAGE_API_KEY;
+const PRIVATE_KEY = process.env.NEXT_MINTER_PRIVATE_KEY;
 
 const client = new NFTStorage({ token: API_KEY! });
 export const isDevelopmentEnvironment = process.env.NODE_ENV !== "production";
@@ -17,9 +22,8 @@ async function storeNft(
   score: number,
   address: string,
 ) {
-  const arrayBuffer = await image.arrayBuffer();
   const nft = {
-    image: new Blob([arrayBuffer]),
+    image,
     name: "RegenScore Card",
     description: "Welcome Regen. Thank you for collecting your Regen Card.",
     properties: {
@@ -40,14 +44,29 @@ type Request = {
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const body = req.body as Request;
 
+  /* TODO: fetch score here */
+  const score = 1283;
+
+  const image = new Blob([await createSvg(score, body.address)], { type: "image/svg+xml" });
+
   const contract = new ethers.Contract(
     process.env.NEXT_PUBLIC_NFT_CONTRACT!,
     nftContractAbi.abi,
   );
+  const metadata = await storeNft(image, score, body.address);
+  const provider = ethers.getDefaultProvider("polygon");
+  const minterWallet = new Wallet(PRIVATE_KEY!, provider);
+  const nftContract = new ethers.Contract(
+    CONTRACT_ADDRESS!,
+    new Interface(abi.abi),
+    minterWallet,
+  );
+  const transaction = await nftContract.safeMint(
+    body.address,
+    metadata.ipnft,
+  );
 
-  /* Dynamically create the nft svg here */
-
-  /* Return the transaction and nft id */
+  return res.status(201).json({ metadata, hash: transaction.hash });
 
   try {
   } catch (e) {
