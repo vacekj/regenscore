419e080a905e4aa49d0d2fe50e5a0a6a4931b450
b3nik <jan.benik47@gmail.com>
2023-08-14 01:30:36 +0200

    custom chakra theme and components

---
+++ src/pages/showcase.tsx
@@ -0,0 +1,18 @@
+import { Check } from "@/components/Check";
+import { Button, Container, Heading } from "@chakra-ui/react";
+
+export default function Showcase() {
+  return (
+    <Container>
+      <Button variant="brand">connect wallet</Button>
+      <Button variant="variant1">connect wallet</Button>
+      <Heading as="h1" variant="h1">Heading H1</Heading>
+      <Heading as="h2" variant="h2">Heading H2</Heading>
+      <Heading as="h3" variant="h3">Heading H3</Heading>
+      <p>
+        Here is our <a href="https://example.com">link!</a>.
+      </p>
+      <Check />
+    </Container>
+  );
+}
