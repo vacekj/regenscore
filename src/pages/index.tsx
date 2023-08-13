08d41eb39bce593bb74a375dda5ee4c865022518
b3nik <jan.benik47@gmail.com>
2023-08-14 01:32:48 +0200

    custom chakra theme and components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -70,15 +70,6 @@ export default function Index(props: {
           <Text textAlign={"center"}>
             We are celebrating all regens who contribute to the greater good. Discover your score below.
           </Text>
-          <Button variant="brand">connect wallet</Button>
-          <Button variant="variant1">connect wallet</Button>
-          <Heading as="h1" variant="h1">Heading H1</Heading>
-          <Heading as="h2" variant="h2">Heading H2</Heading>
-          <Heading as="h3" variant="h3">Heading H3</Heading>
-          <p>
-            Here is our <a href="https://example.com">link!</a>.
-          </p>
-          <Check />
         </Stack>
         <Card />
         {
