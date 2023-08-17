31414c318d5e888ccf9cb1d1339f2a0516483525
mateo daza <mateodaza@gmail.com>
2023-08-17 13:42:49 -0500

    animation on drops added

--- src/components/Landing/HeroSlider.tsx
+++ src/components/Landing/HeroSlider.tsx
@@ -10,38 +10,49 @@ import {
 import { useState } from 'react';
 
 const Slide1 = () => {
-  const images = [
+  const [currentDrop, setCurrentDrop] = useState<number | null>(null);
+  const levitationStyles = ['levitation1', 'levitation2', 'levitation3'];
+
+  const dropsImages = [
     {
       src: '/images/trusted-seed-logo.svg',
       alt: 'trustedseed',
-      top: '10%',
-      left: '22%',
-      width: { base: 93 },
-      height: { base: 93 },
+      top: '13%',
+      left: '24%',
+      width: { base: 33 },
+      height: { base: 33 },
+      dropSize: { base: 93 },
+      content: 'Some more content for Trustedseed',
     },
     {
       src: '/images/giveth-logo.svg',
       alt: 'giveth',
       top: '10%',
       right: '5%',
-      width: { base: 141 },
-      height: { base: 141 },
+      width: { base: 67 },
+      height: { base: 67 },
+      dropSize: { base: 141 },
+      content: 'Some more content for Giveth',
     },
     {
       src: '/images/optimism-logo.svg',
       alt: 'optimism',
       right: { base: '2%', xl: '15%' },
       bottom: { base: 0, xl: '10%' },
-      width: { base: 151, xl: 217 },
-      height: { base: 151, xl: 217 },
+      width: { base: 64, xl: 115 },
+      height: { base: 64, xl: 115 },
+      dropSize: { base: 151, xl: 217 },
+      content: 'Some more content for Optimism',
     },
     {
       src: '/images/gitcoin-logo.svg',
       alt: 'gitcoin',
-      bottom: '-10',
+      bottom: '-6',
       right: '50%',
-      width: { base: 115, lg: 161 },
-      height: { base: 115, lg: 161 },
+      width: { base: 54, lg: 62 },
+      height: { base: 54, lg: 62 },
+      dropSize: { base: 115, lg: 120 },
+      content: 'Some more content for Gitcoin',
     },
   ];
   return (
@@ -58,16 +69,43 @@ const Slide1 = () => {
       justifyContent='center'
       position='relative'
     >
-      {images.map((image, index) => (
-        <Box key={index} position='absolute' blur={10} {...image} zIndex={4}>
-          <Image
-            src={image.src}
-            alt={image.alt}
-            width={image.width}
-            height={image.height}
-          />
-        </Box>
+      {dropsImages.map((image, index) => (
+        <Flex
+          key={index}
+          position='absolute'
+          zIndex={4}
+          {...image}
+          width={image.dropSize}
+          height={image.dropSize}
+          transition={'all 0.4s ease'}
+          onMouseEnter={() => setCurrentDrop(index)}
+          onMouseLeave={() => setCurrentDrop(null)}
+          _hover={{ transform: 'scale(1.1)' }}
+        >
+          <Flex
+            justifyContent='center'
+            bg='rgba(255, 255, 255, 0.2)'
+            boxShadow={'0 4px 30px rgba(0, 0, 0, 0.1)'}
+            border='1px solid rgba(255, 255, 255, 0.3)'
+            borderRadius='50%'
+            backdropFilter={'blur(4px)'}
+            alignItems='center'
+            width={image.dropSize}
+            height={image.dropSize}
+            animation={`${
+              levitationStyles[index % levitationStyles.length]
+            } 5s infinite`}
+          >
+            <Image
+              src={image.src}
+              alt={image.alt}
+              width={image.width}
+              height={image.height}
+            />
+          </Flex>
+        </Flex>
       ))}
+
       <Box
         position='absolute'
         top='0'
@@ -82,6 +120,7 @@ const Slide1 = () => {
         variant='h1'
         color='brand.primaryOrange.300'
         maxWidth={'1100px'}
+        minHeight='92px'
         textAlign='left'
         zIndex={2}
         mb={'48px'}
@@ -94,11 +133,14 @@ const Slide1 = () => {
         variant='xLarge'
         color='white'
         width={{ base: '100%', md: '100%', lg: '800px' }}
+        minHeight='115px'
         textAlign='initial'
         zIndex={2}
         fontSize={{ base: '18px', md: '32px', lg: '44px' }}
       >
-        Qualify for Optimism’s Citizen’s House based on your blockchain activity
+        {currentDrop !== null
+          ? dropsImages[currentDrop].content
+          : 'Qualify for Optimism’s Citizen’s House based on your blockchain activity'}{' '}
       </Text>
     </Flex>
   );
