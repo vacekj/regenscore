39b578f9c42a2c4bdbcd908223a860c66650a5c1
Kilter <mateodaza@gmail.com>
2023-08-30 06:40:15 -0500

    hotfix debugging on demo page

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,18 +1,23 @@
-import { Hex } from "viem";
-import useSWR from "swr";
+import { Hex } from 'viem';
+import useSWR from 'swr';
 
 export function useScore(address: Hex | undefined) {
   const res = useSWR([address], async ([address]) => {
-    const res = await fetch("/api/score", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        address,
-      }),
-    });
-    return res.json();
+    try {
+      const res = await fetch('/api/score', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          address,
+        }),
+      });
+      return res.json();
+    } catch (error) {
+      console.log({ error });
+      return { error };
+    }
   });
 
   return { score: res.data?.score, debug: res.data?.debug, ...res };
