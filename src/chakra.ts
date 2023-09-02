4b7467220c192e87f5da34d575c3f1cc8ddcdca2
Kilter <mateodaza@gmail.com>
2023-09-02 12:42:32 -0500

    chore: adding prettier config

--- src/chakra.ts
+++ src/chakra.ts
@@ -1,136 +1,136 @@
-import { extendTheme } from "@chakra-ui/react";
+import { extendTheme } from '@chakra-ui/react'
 
 export const theme = extendTheme({
-  colors: {
-    brand: {
-      deepGreen: {
-        400: "#354728",
-      },
-      greenLeaf: {
-        400: "#01966B",
-      },
-      limeGreen: {
-        400: "#C2ECBF",
-      },
-      primaryOrange: {
-        300: "#FFC555",
-        200: "#F5B333",
-        100: "#F9DD94",
-      },
-      backgroundOrange: {
-        400: "#F2EFE5",
-      },
-      beige: {
-        400: "rgba(248, 246, 240,1)",
-      },
-    },
-  },
-
-  components: {
-    Button: {
-      baseStyle: {
-        fontSize: "16px",
-        fontFamily: "Inter-Black",
-        textTransform: "uppercase",
-        backgroundColor: "brand.primaryOrange.200",
-        _hover: {
-          backgroundColor: "#D2900F",
-        },
-        _disabled: {
-          backgroundColor: "brand.primaryOrange.100",
-          cursor: "not-allowed",
-        },
-      },
-      variants: {
+    colors: {
         brand: {
-          // Default
-          borderRadius: "0.25rem", // border radius 4px
+            deepGreen: {
+                400: '#354728',
+            },
+            greenLeaf: {
+                400: '#01966B',
+            },
+            limeGreen: {
+                400: '#C2ECBF',
+            },
+            primaryOrange: {
+                300: '#FFC555',
+                200: '#F5B333',
+                100: '#F9DD94',
+            },
+            backgroundOrange: {
+                400: '#F2EFE5',
+            },
+            beige: {
+                400: 'rgba(248, 246, 240,1)',
+            },
         },
-        variant1: {
-          // Metamask
-          width: "249px",
-          height: "64px",
-          minWidth: "192px",
-          color: "brand.deepGreen.400",
-          px: "2rem",
-          borderRadius: "6.25rem", // border radius 100px
-        },
-        variant2: {
-          // WalletConnect
-          backgroundColor: "#F6851B",
-          _hover: {
-            backgroundColor: "#BF5D00",
-          },
-          _disabled: {
-            backgroundColor: "brand.primaryOrange.100",
-            cursor: "not-allowed",
-          },
-        },
-      },
-      defaultProps: {
-        size: "lg",
-        variant: "brand",
-        colorScheme: "",
-      },
     },
-    Heading: {
-      baseStyle: {
-        fontFamily: "Remixa-Medium",
-        fontWeight: "500",
-        lineHeight: "106px",
-      },
-      variants: {
-        h1: {
-          fontSize: "92px",
+
+    components: {
+        Button: {
+            baseStyle: {
+                fontSize: '16px',
+                fontFamily: 'Inter-Black',
+                textTransform: 'uppercase',
+                backgroundColor: 'brand.primaryOrange.200',
+                _hover: {
+                    backgroundColor: '#D2900F',
+                },
+                _disabled: {
+                    backgroundColor: 'brand.primaryOrange.100',
+                    cursor: 'not-allowed',
+                },
+            },
+            variants: {
+                brand: {
+                    // Default
+                    borderRadius: '0.25rem', // border radius 4px
+                },
+                variant1: {
+                    // Metamask
+                    width: '249px',
+                    height: '64px',
+                    minWidth: '192px',
+                    color: 'brand.deepGreen.400',
+                    px: '2rem',
+                    borderRadius: '6.25rem', // border radius 100px
+                },
+                variant2: {
+                    // WalletConnect
+                    backgroundColor: '#F6851B',
+                    _hover: {
+                        backgroundColor: '#BF5D00',
+                    },
+                    _disabled: {
+                        backgroundColor: 'brand.primaryOrange.100',
+                        cursor: 'not-allowed',
+                    },
+                },
+            },
+            defaultProps: {
+                size: 'lg',
+                variant: 'brand',
+                colorScheme: '',
+            },
         },
-        h2: {
-          fontSize: "48px",
-          fontFamily: "Remixa-Semibold",
+        Heading: {
+            baseStyle: {
+                fontFamily: 'Remixa-Medium',
+                fontWeight: '500',
+                lineHeight: '106px',
+            },
+            variants: {
+                h1: {
+                    fontSize: '92px',
+                },
+                h2: {
+                    fontSize: '48px',
+                    fontFamily: 'Remixa-Semibold',
+                },
+                h3: {
+                    fontSize: '24px',
+                    fontFamily: 'Remixa-Bold',
+                },
+            },
         },
-        h3: {
-          fontSize: "24px",
-          fontFamily: "Remixa-Bold",
+        Text: {
+            baseStyle: {
+                fontFamily: 'Inter-Regular',
+                fontSize: '16px',
+                fontWeight: 'normal',
+                cursor: 'default',
+            },
+            variants: {
+                large: {
+                    fontSize: '24px',
+                },
+                xLarge: {
+                    fontSize: '44px',
+                    fontWeigth: '300',
+                    lineHeight: '57px',
+                },
+                bold: {
+                    fontFamily: 'Inter-Bold',
+                    fontWeight: 'bold',
+                },
+                link: {
+                    cursor: 'pointer',
+                    _hover: {
+                        color: 'brand.primaryOrange.200',
+                    },
+                },
+                boldLink: {
+                    fontWeight: 'bold',
+                    cursor: 'pointer',
+                    _hover: {
+                        color: 'brand.primaryOrange.200',
+                    },
+                },
+                largeBold: {
+                    fontSize: '24px',
+                    fontWeight: 'bold',
+                },
+            },
         },
-      },
     },
-    Text: {
-      baseStyle: {
-        fontFamily: "Inter-Regular",
-        fontSize: "16px",
-        fontWeight: "normal",
-        cursor: "default",
-      },
-      variants: {
-        large: {
-          fontSize: "24px",
-        },
-        xLarge: {
-          fontSize: "44px",
-          fontWeigth: "300",
-          lineHeight: "57px",
-        },
-        bold: {
-          fontFamily: "Inter-Bold",
-          fontWeight: "bold",
-        },
-        link: {
-          cursor: "pointer",
-          _hover: {
-            color: "brand.primaryOrange.200",
-          },
-        },
-        boldLink: {
-          fontWeight: "bold",
-          cursor: "pointer",
-          _hover: {
-            color: "brand.primaryOrange.200",
-          },
-        },
-        largeBold: {
-          fontSize: "24px",
-          fontWeight: "bold",
-        },
-      },
-    },
-  },
-});
+})
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,59 +1,55 @@
-import {
-  EthereumClient,
-  w3mConnectors,
-  w3mProvider,
-} from "@web3modal/ethereum";
-import { Web3Modal } from "@web3modal/react";
-import { configureChains, createConfig, WagmiConfig } from "wagmi";
-import { optimism, optimismGoerli } from "wagmi/chains";
+import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
+import { Web3Modal } from '@web3modal/react'
+import { configureChains, createConfig, WagmiConfig } from 'wagmi'
+import { optimism, optimismGoerli } from 'wagmi/chains'
 
-import { AnimatePresence } from "framer-motion";
-import { ChakraProvider } from "@chakra-ui/react";
-import { AppProps } from "next/app";
-import Head from "next/head";
-import { theme } from "@/chakra";
-import "./styles.css";
+import { AnimatePresence } from 'framer-motion'
+import { ChakraProvider } from '@chakra-ui/react'
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+import { theme } from '@/chakra'
+import './styles.css'
 
-const chains = [optimism, optimismGoerli];
-const projectId = "df4f5f1b03670ef123bd5ee18401d0de";
+const chains = [optimism, optimismGoerli]
+const projectId = 'df4f5f1b03670ef123bd5ee18401d0de'
 
-const { publicClient } = configureChains(chains, [w3mProvider({ projectId })]);
+const { publicClient } = configureChains(chains, [w3mProvider({ projectId })])
 const wagmiConfig = createConfig({
-  autoConnect: true,
-  connectors: w3mConnectors({ projectId, chains }),
-  publicClient,
-});
-const ethereumClient = new EthereumClient(wagmiConfig, chains);
+    autoConnect: true,
+    connectors: w3mConnectors({ projectId, chains }),
+    publicClient,
+})
+const ethereumClient = new EthereumClient(wagmiConfig, chains)
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <title>RegenScore</title>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
-        />
-      </Head>
-      <WagmiConfig config={wagmiConfig}>
-        <ChakraProvider theme={theme}>
-          <AnimatePresence mode="wait">
-            <Component {...pageProps} />
-          </AnimatePresence>
-        </ChakraProvider>
-      </WagmiConfig>
-      <Web3Modal
-        projectId={projectId}
-        ethereumClient={ethereumClient}
-        themeVariables={{
-          "--w3m-font-family": "Remixa, sans-serif",
-          "--w3m-accent-color": "black",
-          "--w3m-background-color": "#354728",
-          "--w3m-button-border-radius": "100px",
-        }}
-      />
-    </>
-  );
+    return (
+        <>
+            <Head>
+                <title>RegenScore</title>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
+                />
+            </Head>
+            <WagmiConfig config={wagmiConfig}>
+                <ChakraProvider theme={theme}>
+                    <AnimatePresence mode="wait">
+                        <Component {...pageProps} />
+                    </AnimatePresence>
+                </ChakraProvider>
+            </WagmiConfig>
+            <Web3Modal
+                projectId={projectId}
+                ethereumClient={ethereumClient}
+                themeVariables={{
+                    '--w3m-font-family': 'Remixa, sans-serif',
+                    '--w3m-accent-color': 'black',
+                    '--w3m-background-color': '#354728',
+                    '--w3m-button-border-radius': '100px',
+                }}
+            />
+        </>
+    )
 }
 
-export default MyApp;
+export default MyApp
