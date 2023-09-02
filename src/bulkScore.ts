5def80c7d9dd5b61f3348d387503df1e7630df01
Kilter <mateodaza@gmail.com>
2023-09-02 12:42:46 -0500

    prettifies files

--- src/bulkScore.ts
+++ src/bulkScore.ts
@@ -1,33 +1,33 @@
-import { createScore } from '@/helpers/scoreHelpers'
-import { parse } from 'csv-parse'
-import { stringify } from 'csv-stringify/sync'
-import fs from 'fs'
+import { createScore } from '@/helpers/scoreHelpers';
+import { parse } from 'csv-parse';
+import { stringify } from 'csv-stringify/sync';
+import fs from 'fs';
 
-import { dirname } from 'path'
-import { fileURLToPath } from 'url'
-import { isAddress } from 'viem'
-const __dirname = dirname(fileURLToPath(import.meta.url))
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { isAddress } from 'viem';
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const processFile = async () => {
-    const records = []
-    const parser = fs
-        .createReadStream(`${__dirname}/../data/delegates.csv`)
-        .pipe(parse({}))
-    for await (const record of parser) {
-        const address = record[0]
-        if (!isAddress(address)) {
-            console.warn('Not an address', address)
-            continue
-        }
-        const score = await createScore(address)
-        console.log(record[0], score.score)
-        records.push([...record, score.score])
-    }
-    return records
-}
+	const records = [];
+	const parser = fs
+		.createReadStream(`${__dirname}/../data/delegates.csv`)
+		.pipe(parse({}));
+	for await (const record of parser) {
+		const address = record[0];
+		if (!isAddress(address)) {
+			console.warn('Not an address', address);
+			continue;
+		}
+		const score = await createScore(address);
+		console.log(record[0], score.score);
+		records.push([...record, score.score]);
+	}
+	return records;
+};
 
-;(async () => {
-    const records = await processFile()
-    const csvText = stringify(records)
-    fs.writeFileSync('./delegates_with_score.csv', csvText)
-})()
+(async () => {
+	const records = await processFile();
+	const csvText = stringify(records);
+	fs.writeFileSync('./delegates_with_score.csv', csvText);
+})();
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,31 +1,31 @@
-import React from 'react'
-import { Flex, Box } from '@chakra-ui/react'
-import Footer from './Footer'
-import Header from './Header'
-import PageTransition from './PageTransition'
+import React from 'react';
+import { Flex, Box } from '@chakra-ui/react';
+import Footer from './Footer';
+import Header from './Header';
+import PageTransition from './PageTransition';
 
 type LayoutProps = {
-    children: React.ReactNode
-}
+	children: React.ReactNode;
+};
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
-    return (
-        <PageTransition>
-            <Flex
-                as="main"
-                width="100%"
-                minWidth={'100vw'}
-                flexDirection="column"
-                backgroundColor={'brand.backgroundOrange.400'}
-            >
-                <Header />
-                <Box as="main" minHeight="100vh" flexGrow={1}>
-                    {children}
-                </Box>
-                <Footer />
-            </Flex>
-        </PageTransition>
-    )
-}
+	return (
+		<PageTransition>
+			<Flex
+				as='main'
+				width='100%'
+				minWidth={'100vw'}
+				flexDirection='column'
+				backgroundColor={'brand.backgroundOrange.400'}
+			>
+				<Header />
+				<Box as='main' minHeight='100vh' flexGrow={1}>
+					{children}
+				</Box>
+				<Footer />
+			</Flex>
+		</PageTransition>
+	);
+};
 
-export default Layout
+export default Layout;
