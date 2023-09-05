5eea84319770b64e44600fd7140a62472351daaf
b3nik <jan.benik47@gmail.com>
2023-09-06 01:47:19 +0200

    fix: links

--- src/components/Landing/HowItWorks.tsx
+++ src/components/Landing/HowItWorks.tsx
@@ -52,14 +52,17 @@ const HowItWorks: React.FC = () => {
         <BoxContent
           step={4}
           title="Help us improve"
-          content={(
+          content={
             <>
               Join the Regen Score community and{' '}
               <Text fontWeight="bold" as="span">
-              <a href="https://tally.so/r/w4JpDA">share your feedback and suggestions</a></Text>{' '}
+                <a href="https://tally.so/r/w4JpDA">
+                  share your feedback and suggestions
+                </a>
+              </Text>{' '}
               on how we can make it even better.
             </>
-          )}
+          }
           last={true}
         />
       </Grid>
@@ -128,11 +131,20 @@ const BoxContent = ({
       display="inline-block"
       position="relative"
       pt="70px"
-      
     >
       {title}
     </Text>
-    <Text fontSize="16px" lineHeight="32px" paddingRight={{ base: "0px", sm: "10px", md: "10px", lg: "10px", xl: "10px" }}>
+    <Text
+      fontSize="16px"
+      lineHeight="32px"
+      paddingRight={{
+        base: '0px',
+        sm: '10px',
+        md: '10px',
+        lg: '10px',
+        xl: '10px',
+      }}
+    >
       {content}
     </Text>
   </Flex>
