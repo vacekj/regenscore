a0dc5e0e01ad653d43470803f3834a0947032528
b3nik <jan.benik47@gmail.com>
2023-09-06 00:25:46 +0200

    fix: fix drawer menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -147,13 +147,10 @@ const Header: React.FC = () => {
                 <DrawerHeader>Menu</DrawerHeader>
                 <DrawerBody>
                   <VStack align="start" spacing={4}>
-                    <Link href="/">
-                      <Text variant={'boldLink'}>Opportunities</Text>
-                    </Link>
                     <Link href="/leaderboard">
-                      <Text variant={'boldLink'}>Leaderboard</Text>
+                      <Text variant={'boldLink'}>My Profile</Text>
                     </Link>
-                    <Link href="/docs">
+                    <Link href="https://giveth.notion.site/Docs-Portal-d21fd8ee276f462d93e5a9083ec2ff8d">
                       <Text variant={'boldLink'}>Docs</Text>
                     </Link>
                   </VStack>
