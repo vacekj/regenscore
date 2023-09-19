4ecb6b3fa1dbc9b55f6b7dda71c21d506c0f67ab
Mateo Daza <mateodaza@gmail.com>
2023-09-19 07:43:00 -0500

    fixes time formatting

--- src/utils/strings.ts
+++ src/utils/strings.ts
@@ -1,12 +1,15 @@
 export function formatTimestamp(timestamp: number): string {
-  const date = new Date(timestamp * 1000);
-  const options: Intl.DateTimeFormatOptions = {
-    year: 'numeric',
-    month: 'long',
-    day: '2-digit',
-    hour: '2-digit',
-    minute: '2-digit',
-  };
-
-  return new Intl.DateTimeFormat('en-US', options).format(date);
+  try {
+    const date = new Date(timestamp * 1000);
+    const options: Intl.DateTimeFormatOptions = {
+      year: 'numeric',
+      month: 'long',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+    };
+    return new Intl.DateTimeFormat('en-US', options)?.format(date);
+  } catch (error) {
+    return '';
+  }
 }
