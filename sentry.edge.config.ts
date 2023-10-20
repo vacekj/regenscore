efb7ec80ced5372accb2b558b12b2bbc2b20e132
b3nik <jan.benik47@gmail.com>
2023-10-20 15:15:40 +0200

    disable sentry for development

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -5,12 +5,14 @@
 
 import * as Sentry from '@sentry/nextjs';
 
-Sentry.init({
-  dsn: 'https://5d17f5f5b130accef2c77ac81963e5af@o134142.ingest.sentry.io/4505919384059904',
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    dsn: 'https://5d17f5f5b130accef2c77ac81963e5af@o134142.ingest.sentry.io/4505919384059904',
 
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+    // Adjust this value in production, or use tracesSampler for greater control
+    tracesSampleRate: 1,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
-});
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
+  });
+}
