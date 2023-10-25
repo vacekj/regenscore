a2bf3b5602bfc1f5ae91d8fafcd20388b7c4e764
b3nik <b3nik@proton.me>
2023-10-26 01:36:38 +0200

    clean footer and category section

---
+++ src/components/SVG-components/Rectangle.tsx
@@ -0,0 +1,26 @@
+const Rectangle = () => (
+  <svg
+    width="80"
+    height="19"
+    viewBox="0 0 80 19"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect width="80" height="19" fill="url(#paint0_linear_847_5847)" />
+    <defs>
+      <linearGradient
+        id="paint0_linear_847_5847"
+        x1="76.8542"
+        y1="-2.00987e-06"
+        x2="39.0971"
+        y2="50.4459"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#F2EFE5" />
+        <stop offset="1" stopColor="#F3FFDA" />
+      </linearGradient>
+    </defs>
+  </svg>
+);
+
+export default Rectangle;
---
+++ src/components/SVG-components/Twitter.tsx
@@ -0,0 +1,18 @@
+const Twitter = () => (
+  <svg
+    id="Layer_1"
+    data-name="Layer 1"
+    width="24px"
+    height="24px"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 487.43 456.19"
+  >
+    <path
+      id="path1009"
+      d="M7.48,21.9,195.66,273.57,6.29,478.1H48.91L214.71,299l134,179.11h145L294.93,212.33,471.2,21.9H428.58L275.89,186.82,152.51,21.9ZM70.16,53.3h66.63L431,446.7H364.39Z"
+      transform="translate(-6.29 -21.9)"
+    />
+  </svg>
+);
+
+export default Twitter;
