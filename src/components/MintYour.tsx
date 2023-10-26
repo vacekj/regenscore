e6a12e0c4b98627e753fd945a701c30df24a9e64
b3nik <b3nik@proton.me>
2023-10-26 02:31:08 +0200

    svg react components

--- src/components/MintYour.tsx
+++ src/components/MintYour.tsx
@@ -29,6 +29,7 @@ import { useScoreContext } from '@/contexts/scoreContext';
 import CategoriesSection from './CategorySection';
 import { CURRENT_SCORE_VERSION } from '@/constants';
 import { IDatabaseMeta } from '@/helpers/sourceTypes';
+import AttestationFailed from './SVG-components/AttestationFailed';
 
 interface IMintYour {
   _address?: Hex;
@@ -585,18 +586,3 @@ const MintYour = ({ _address }: IMintYour) => {
 };
 
 export default MintYour;
-
-const AttestationFailed = () => (
-  <svg
-    width="52"
-    height="52"
-    viewBox="0 0 52 52"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      d="M26 0C11.6637 0 0 11.6637 0 26C0 40.3363 11.6637 52 26 52C40.3363 52 52 40.3363 52 26C52 11.6637 40.3363 0 26 0ZM28.5 39.9888H23.5V34.9888H28.5V39.9888ZM28 32H24L23.25 12H28.75L28 32Z"
-      fill="#E43126"
-    />
-  </svg>
-);
--- src/components/YourScore.tsx
+++ src/components/YourScore.tsx
@@ -23,6 +23,7 @@ import { Check } from '@/components/Check';
 import { Hex } from 'viem';
 import { formatNumber } from '@/utils/strings';
 import CategoriesSection from './CategorySection';
+import AttestationFailed from './SVG-components/AttestationFailed';
 
 interface IMintYour {
   _address: Hex;
@@ -443,18 +444,3 @@ const MintYour = ({ _address }: IMintYour) => {
 };
 
 export default MintYour;
-
-const AttestationFailed = () => (
-  <svg
-    width="52"
-    height="52"
-    viewBox="0 0 52 52"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      d="M26 0C11.6637 0 0 11.6637 0 26C0 40.3363 11.6637 52 26 52C40.3363 52 52 40.3363 52 26C52 11.6637 40.3363 0 26 0ZM28.5 39.9888H23.5V34.9888H28.5V39.9888ZM28 32H24L23.25 12H28.75L28 32Z"
-      fill="#E43126"
-    />
-  </svg>
-);
